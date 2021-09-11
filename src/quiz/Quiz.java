9429f6848f83fdacefce486102c4f8522b9e3151
anton_balakirev <anton_balakirev@epage.ru>
2021-09-11 10:51:54 +0300

    optimize code

--- src/quiz/Quiz.java
+++ src/quiz/Quiz.java
@@ -2,12 +2,15 @@ package quiz;
 
 import java.util.Scanner;
 
-import static java.lang.System.*;
-
 public class Quiz {
     public static String[] q = new String[10];
     public static String[] a = new String[10];
-    public static void main(String... args) {
+
+    public static String getUserInput() {
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static void fillQuestions() {
         q[0] = "1.Wer ist der Mann? Kennst du ____?\na.er\nb.ihm\nc.ein\nd.ihn";
         q[1] = "2.Ich trinke Saft. Und was __________ ?\na.du\nb.trinkst du\nc.hast du\nd.wirst du";
         q[2] = "3.Möchtest du heute Abend ____ Film sehen?\na.das\nb.eine\nc.einen\nd.dem";
@@ -20,34 +23,42 @@ public class Quiz {
         q[8] =  "9.Der Unterricht beginnt __ Montag um 10 Uhr.\na.am\nb.um\nc.im\nd.an";
         q[9] = "10.Woher kommen Sie?\na.Ich wohne in Berlin.\n" +
                 "b.Ich komme bald nach Berlin.\nc.Regina kommt auch aus München.\nd.Aus Deutschland, aus Berlin.";
+    }
+
+    public static void fillAnswers() {
         String[] a = {"d", "b", "c", "b", "a", "c", "a", "b", "a", "d"};
-        String welcome = "Hello! Welcome to our German quiz. If you want to beginn, press s, exit - press e.";
-        out.println(welcome);
-        Scanner read = new Scanner(in);
-        String start = read.nextLine();
-        switch (start){
-            case "e":
-                out.println("See you next time.");
-                break;
-            case "s":
-                int i = 0;
-                int y = 0;
-                int scoore = 0;
-                while (i < q.length && y < a.length) {
-                    out.println(q[i]);
-                    String answer = read.nextLine();
-                    if (answer.equals(a[y])) {
-                        System.out.println("Right");
-                        scoore++;
-                    } else {
-                        System.out.println("False");
-                    }i++;
-                    y++;
-                }System.out.println("You answered " + scoore + "/10 quetions.");
-                break;
-            default:
-                out.println("Wrong choise");
-                break;
+    }
+
+    public static int play() {
+        int i = 0;
+        int y = 0;
+        int scoore = 0;
+        while (i < q.length && y < a.length) {
+            System.out.println(q[i]);
+            String answer = getUserInput();
+            if (answer.equals(a[y])) {
+                System.out.println("Right");
+                scoore++;
+            } else {
+                System.out.println("False");
+            }
+            i++;
+            y++;
+        }
+        return scoore;
+    }
+
+    public static void main(String... args) {
+        fillQuestions();
+        fillAnswers();
+        System.out.println("Hello! Welcome to our German quiz. If you want to beginn, press s, exit - press e.");
+        switch (getUserInput()) {
+            case "e" -> System.out.println("See you next time.");
+            case "s" -> {
+                int scoore = play();
+                System.out.println("You answered " + scoore + "/10 quetions.");
+            }
+            default -> System.out.println("Wrong choise");
         }
     }
 }
